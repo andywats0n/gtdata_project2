5e791273fc6c131891b013414764fb9af8879187
andy <4ndy.watson@gmail.com>
2019-01-05 20:39:43 -0500

    added test route to serve api endpoint

--- src/app.py
+++ src/app.py
@@ -15,5 +15,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html', payload=payload)
 
+@app.route('/api/test', methods=['GET','POST'])
+def api_test():
+    return 'test'
+
 if __name__ == "__main__":
     app.run(debug=True)
--- src/static/index.js
+++ src/static/index.js
@@ -1,7 +1,22 @@
 
-let mymap = L.map('mapid').setView([51.505, -0.09], 13);
+let mymap = L.map('mapid').setView([33, -84], 13);
 
-L.tileLayer(`https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=${API_KEY}`, {
-    maxZoom: 18,
+let mapUrl = `https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=${API_KEY}`
+
+L.tileLayer(mapUrl, {
+    maxZoom: 7,
     id: 'mapbox.streets',
 }).addTo(mymap);
+
+mymap.addEventListener('click', e => console.log(e))
+
+
+let lat = '3.11'
+let lng = ''
+// let url = `http://api.aerisapi.com/version?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`
+let url = `https://api.aerisapi.com/lightning/closest?p=minneapolis,mn&format=json&radius=1000mi&filter=cg&limit=100&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`
+
+fetch(url)
+  .then(res => res.json())
+  .then(data => console.log(data))
+
