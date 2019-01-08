bca240b3ab44806ea9cab8e8aa3d2cf1c63ecf42
anikara <42068814+anikara@users.noreply.github.com>
2019-01-08 18:50:47 -0500

    initial

---
+++ src/Project3.py
@@ -0,0 +1,146 @@
+
+
+# Dependencies and Setup
+import pandas as pd
+import numpy as np
+import requests
+import time
+import datetime as dt
+
+# Import API key
+from api_keys import CLIENT_ID, CLIENT_SECRET
+
+
+
+
+# boundaries
+bottom = 25
+top = 49
+left = -126
+right = -66
+add_prcp = "&filter=prcp"
+add_snow= "&filter=snow"
+
+
+
+df_maxtemp = pd.DataFrame([], columns = {'Date','Date(ISO)', 'Lat', 'Long', 'Temp(F)'})
+df_mintemp = pd.DataFrame([], columns = {'Date','Date(ISO)', 'Lat', 'Long', 'Temp(F)'})
+df_prcp = pd.DataFrame([], columns = {'Date','Date(ISO)', 'Lat', 'Long', 'Prcp(in)'})
+df_snow = pd.DataFrame([], columns = {'Date','Date(ISO)', 'Lat', 'Long', 'Snow(in)'})
+
+df_snow = df_snow[['Date','Date(ISO)',  'Lat', 'Long', 'Snow(in)']]
+df_prcp = df_prcp[['Date','Date(ISO)',  'Lat', 'Long', 'Prcp(in)']]
+df_maxtemp = df_maxtemp[['Date', 'Date(ISO)', 'Lat', 'Long', 'Temp(F)']]
+df_mintemp = df_mintemp[['Date', 'Date(ISO)', 'Lat', 'Long', 'Temp(F)']]
+
+height = (top-bottom)/15
+width = (right-left)/40
+
+        
+
+##MAXTEMP
+for i in range(15):
+    box_top = round(top - i*height,4)
+    box_bottom = round(box_top - height,4)
+    for j in range(40):
+        box_left = round(left + j*height,4)
+        box_right = round(box_left + width,4)
+        
+        query_url = f"https://api.aerisapi.com/records/within?p={box_bottom},{box_left},{box_top},{box_right}&limit=2000&from=01/01/1950&to=now&client_id={CLIENT_ID}&client_secret={CLIENT_SECRET}&filter=maxt"
+        weather_response = requests.get(query_url)
+        weather_json = weather_response.json()
+        print(i*40+j, len(weather_json['response']), box_bottom, box_left, box_top, box_right)
+        for k in range(len(weather_json['response'])):
+            response = weather_json['response'][k]
+
+            fahr_temp = response['report']['details']['tempF']
+            lat = response['loc']['lat']
+            lon = response['loc']['long']
+            date = response['report']['timestamp']
+            dateiso = response['report']['dateTimeISO']
+            
+            df_maxtemp = df_maxtemp.append(pd.Series([date, dateiso, lat, lon, fahr_temp], index=df_maxtemp.columns ), ignore_index=True)
+       
+
+
+
+
+##MINTEMP
+for i in range(15):
+    box_top = round(top - i*height,4)
+    box_bottom = round(box_top - height,4)
+    for j in range(40):
+        box_left = round(left + j*height,4)
+        box_right = round(box_left + width,4)
+        
+        query_url = f"https://api.aerisapi.com/records/within?p={box_bottom},{box_left},{box_top},{box_right}&limit=2000&from=01/01/1950&to=now&client_id={CLIENT_ID}&client_secret={CLIENT_SECRET}&filter=mint"
+        weather_response = requests.get(query_url)
+        weather_json = weather_response.json()
+        print(i*40+j, len(weather_json['response']), box_bottom, box_left, box_top, box_right)
+        for k in range(len(weather_json['response'])):
+            response = weather_json['response'][k]
+
+            fahr_temp = response['report']['details']['tempF']
+            lat = response['loc']['lat']
+            lon = response['loc']['long']
+            date = response['report']['timestamp']
+            dateiso = response['report']['dateTimeISO']
+            
+            df_mintemp = df_mintemp.append(pd.Series([date, dateiso, lat, lon, fahr_temp], index=df_mintemp.columns ), ignore_index=True)
+       
+
+
+
+##PRCP
+for i in range(15):
+    box_top = round(top - i*height,4)
+    box_bottom = round(box_top - height,4)
+    for j in range(40):
+        box_left = round(left + j*height,4)
+        box_right = round(box_left + width,4)
+        
+        query_url = f"https://api.aerisapi.com/records/within?p={box_bottom},{box_left},{box_top},{box_right}&limit=2000&from=01/01/2000&to=now&client_id={CLIENT_ID}&client_secret={CLIENT_SECRET}&filter=prcp"
+        weather_response = requests.get(query_url)
+        weather_json = weather_response.json()
+        print(i*40+j, len(weather_json['response']), box_bottom, box_left, box_top, box_right)
+        for k in range(len(weather_json['response'])):
+            
+            response = weather_json['response'][k]
+
+            prcp = response['report']['details']['rainIN']
+            lat = response['loc']['lat']
+            lon = response['loc']['long']
+            date = response['report']['timestamp']
+            dateiso = response['report']['dateTimeISO']
+            
+            df_prcp = df_prcp.append(pd.Series([date, dateiso, lat, lon, prcp], index=df_prcp.columns ), ignore_index=True)
+       
+
+
+
+
+##SNOW
+for i in range(15):
+    box_top = round(top - i*height,4)
+    box_bottom = round(box_top - height,4)
+    for j in range(40):
+        box_left = round(left + j*height,4)
+        box_right = round(box_left + width,4)
+        
+        query_url = f"https://api.aerisapi.com/records/within?p={box_bottom},{box_left},{box_top},{box_right}&limit=2000&from=01/01/2000&to=now&client_id={CLIENT_ID}&client_secret={CLIENT_SECRET}&filter=snow"
+        weather_response = requests.get(query_url)
+        weather_json = weather_response.json()
+        print(i*40+j, len(weather_json['response']), box_bottom, box_left, box_top, box_right)
+        for k in range(len(weather_json['response'])):
+            response = weather_json['response'][k]
+
+            snow = response['report']['details']['snowIN']
+            lat = response['loc']['lat']
+            lon = response['loc']['long']
+            date = response['report']['timestamp']
+            dateiso = response['report']['dateTimeISO']
+            
+            df_snow = df_snow.append(pd.Series([date, dateiso lat, lon, snow], index=df_snow.columns ), ignore_index=True)
+       
+
+
--- src/static/index.js
+++ src/static/index.js
@@ -5,3 +5,12 @@ L.tileLayer(`https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=$
     maxZoom: 18,
     id: 'mapbox.streets',
 }).addTo(mymap);
+
+let url = `https://api.aerisapi.com/records/within?p=47,-122,48,-123&filter=prcp&limit=2000&from=01/01/2000&to=now&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`
+
+
+//let url = `https://api.aerisapi.com/records/minneapolis,mn?from=-1year&filter=prcp&limit=100&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`
+
+fetch(url)
+  .then(res => res.json())
+  .then(data => console.log(data))
