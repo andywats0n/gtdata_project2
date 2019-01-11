2a760ccf13ddcc89e7258c5d02a989aee255338d
David Shaffer <dshaffer8641@gmail.com>
2019-01-11 18:09:42 -0500

    updates to tilelayers

--- src/static/index.js
+++ src/static/index.js
@@ -4,11 +4,14 @@ const center = [39.8283, -98.5795]
 let mymap = L.map('mapid').setView(center, 5.15);
 
 let mapUrl = `https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=${API_KEY}`
-let url = 'http://localhost:5000/api/test'
+let precipUrl = 'http://localhost:5000/?'
+let snowUrl = 'http://localhost:5000/?'
+let mintempUrl = 'http://localhost:5000/?'
+let maxtempUrl = 'http://localhost:5000/?'
 
 primeTile = L.tileLayer(mapUrl, { id: 'mapbox.streets' })
 
-primeTile.addTo(mymap);
+// primeTile.addTo(mymap);
 
 mymap.addEventListener('click', e => {console.log(e.latlng.lat, e.latlng.lng)});
 
