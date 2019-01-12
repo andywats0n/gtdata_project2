2d20e2de75d2937173fedfdebd797c37e8c8ecf9
anikara <42068814+anikara@users.noreply.github.com>
2019-01-11 21:58:14 -0500

    js changes

--- src/static/index.js
+++ src/static/index.js
@@ -14,23 +14,26 @@ mymap.addEventListener('click', e => {console.log(e.latlng.lat, e.latlng.lng)})
 //   .then(res => res.json())
 //   .then(data => console.log(data))
 
-let myurl = 'http://localhost:5000/api/maxt';
+let myurl = '/api/filtermaxt/' + '7/2018';
 d3.json(myurl, function(response) {
 
   console.log('reading');
   
   var heatArray = [];
-  
+  var radii = [];
   for (var i = 0; i < response.length; i++) {
     var record = response[i];
   
-        heatArray.push([record.Lat, record.Long, record['Temp(F)']]);
-      
+        heatArray.push([record.Lat, record.Long, record['Temp(F)']*2]);
+        //radii.push(25 + record['Snow(in)']*5)
+    
+
     }
  // console.log(heatArray)
   var heat = L.heatLayer(heatArray, {
-      radius: 50,
-      blur: 15
+      radius: 25,
+      blur: 5
+      //gradient: {0.3: 'blue', .6: 'lime', 1: 'red'}
   }).addTo(mymap);
   console.log('done');
 });
