5ebcaf6d3eeae34058db8576be300b0a07e9aaa2
David Shaffer <dshaffer8641@gmail.com>
2019-01-12 02:47:38 -0500

    i think im close

--- src/static/index.js
+++ src/static/index.js
@@ -1,100 +1,146 @@
-const center = [39.8283, -98.5795];
-let mapUrl = `https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=${API_KEY}`;
-let month = 8;
-let year = 2017;
+// function init() {
+//
+//   const center = [39.8283, -98.5795];
+//   let mapUrl = `https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=${API_KEY}`;
+//   let month = 8;
+//   let year = 2017;
+//
+//   var precipArray = [];
+//   var snowArray = [];
+//   var mintempArray = [];
+//   var maxtempArray = [];
+//
+//   let precipLayer = L.layerGroup(precipArray);
+//   let snowLayer = L.layerGroup(snowArray);
+//   let mintempLayer = L.layerGroup(mintempArray);
+//   let maxtempLayer = L.layerGroup(maxtempArray);
+//
+//   let overlayMaps = {
+//     Precipitation: precipLayer,
+//     Snow: snowLayer,
+//     MinTemperatures: mintempLayer,
+//     MaxTemperatures: maxtempLayer
+//   };
+//
+//   let primeTile = L.tileLayer(mapUrl, { id: 'mapbox.streets' });
+//
+//   let baseMaps = {
+//     "Base Map": primeTile
+//   };
+//
+//   let map = L.map('mapid', {
+//     center:center,
+//     zoom: 5.15,
+//     layers: [primeTile]
+//   });
+// };
 
-var precipArray = [];
-var snowArray = [];
-var mintempArray = [];
-var maxtempArray = [];
 
-let precipLayer = L.layerGroup(precipArray);
-let snowLayer = L.layerGroup(snowArray);
-let mintempLayer = L.layerGroup(mintempArray);
-let maxtempLayer = L.layerGroup(maxtempArray);
+function getData(dataset) {
 
-let overlayMaps = {
-  Precipitation: precipLayer,
-  Snow: snowLayer,
-  MinTemperatures: mintempLayer,
-  MaxTemperatures: maxtempLayer
-};
+  const center = [39.8283, -98.5795];
+  let mapUrl = `https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=${API_KEY}`;
+  let month = 1;
+  let year = 2017;
 
-let primeTile = L.tileLayer(mapUrl, { id: 'mapbox.streets' });
+  var precipArray = [];
+  var snowArray = [];
+  var mintempArray = [];
+  var maxtempArray = [];
 
-let baseMaps = {
-  "Base Map": primeTile
-};
+  let precipLayer = L.layerGroup(precipArray);
+  let snowLayer = L.layerGroup(snowArray);
+  let mintempLayer = L.layerGroup(mintempArray);
+  let maxtempLayer = L.layerGroup(maxtempArray);
 
-let map = L.map('mapid', {
-  center:center,
-  zoom: 5.15,
-  layers: [primeTile, snowLayer]
-});
+  let overlayMaps = {
+    Precipitation: precipLayer,
+    Snow: snowLayer,
+    MinTemperatures: mintempLayer,
+    MaxTemperatures: maxtempLayer
+  };
 
+  let primeTile = L.tileLayer(mapUrl, { id: 'mapbox.streets' });
+
+  let baseMaps = {
+    "Base Map": primeTile
+  };
+
+  let map = L.map('mapid', {
+    center:center,
+    zoom: 5.15,
+    layers: [primeTile]
+  });
 // precipitation
-let precipUrl = `http://localhost:5000/api/filterprcp/${month}/${year}`
-d3.json(precipUrl, function(response) {
 
-  for (var i = 0; i < response.length; i++) {
-    var record = response[i];
-    precipArray.push([record.Lat, record.Long, record['Prcp(in)']]);
-  }
-    var heat = L.heatLayer(precipArray, {
-        radius: 30,
-        blur: 15,
-        gradient: {0.3: 'blue', .5: 'lime', 1: 'red'}
-      }).addTo(map);
-    });
+if (dataset === 'prcp') {
+  let precipUrl = `http://localhost:5000/api/filterprcp/${month}/${year}`
+  d3.json(precipUrl, function(response) {
 
+    for (var i = 0; i < response.length; i++) {
+      var record = response[i];
+      precipArray.push([record.Lat, record.Long, record['Prcp(in)']]);
+    }
+      var heat = L.heatLayer(precipArray, {
+          radius: 30,
+          blur: 15,
+          gradient: {0.3: 'blue', .5: 'lime', 1: 'red'}
+        }).addTo(map);
+      });
+};
 // snow
-let snowUrl = `http://localhost:5000/api/filtersnow/${month}/${year}`
-d3.json(snowUrl, function(response) {
-
-  for (var i = 0; i < response.length; i++) {
-    var record = response[i];
-    snowArray.push([record.Lat, record.Long, record['Snow(in)']]);
-  }
-    var heat = L.heatLayer(snowArray, {
-        radius: 30,
-        blur: 15,
-        gradient: {0.3: 'blue', .7: 'lime', 1: 'red'}
-      }).addTo(map);
-    });
-
+if (dataset === 'snow') {
+  let snowUrl = `http://localhost:5000/api/filtersnow/${month}/${year}`
+  d3.json(snowUrl, function(response) {
+    console.log(response.length);
+    for (var i = 0; i < response.length; i++) {
+      var record = response[i];
+      snowArray.push([record.Lat, record.Long, record['Snow(in)']]);
+    }
+      var heat = L.heatLayer(snowArray, {
+          radius: 30,
+          blur: 15,
+          gradient: {0.3: 'blue', .7: 'lime', 1: 'red'}
+        }).addTo(map);
+      });
+};
 // mintemp
-let mintempUrl = `http://localhost:5000/api/filtermint/${month}/${year}`
-d3.json(mintempUrl, function(response) {
-
-  for (var i = 0; i < response.length; i++) {
-    var record = response[i];
-    mintempArray.push([record.Lat, record.Long, record['Temp(F)']]);
-  }
-    var heat = L.heatLayer(mintempArray, {
-        radius: 20,
-        blur: 20,
-        gradient: {0.4: 'blue', .8: 'lime', 1: 'red'}
-      }).addTo(map);
-    });
-
+if (dataset === 'mint') {
+  let mintempUrl = `http://localhost:5000/api/filtermint/${month}/${year}`
+  d3.json(mintempUrl, function(response) {
+    console.log(mintempUrl.slice(32,36))
+    for (var i = 0; i < response.length; i++) {
+      var record = response[i];
+      mintempArray.push([record.Lat, record.Long, record['Temp(F)']]);
+    }
+      var heat = L.heatLayer(mintempArray, {
+          radius: 20,
+          blur: 20,
+          gradient: {0.4: 'blue', .8: 'lime', 1: 'red'}
+        }).addTo(map);
+      });
+};
 // maxtemp
-let maxtempUrl = `http://localhost:5000/api/filtermaxt/${month}/${year}`
-d3.json(maxtempUrl, function(response) {
+if (dataset === 'maxt') {
+  let maxtempUrl = `http://localhost:5000/api/filtermaxt/${month}/${year}`
+  d3.json(maxtempUrl, function(response) {
 
-  for (var i = 0; i < response.length; i++) {
-    var record = response[i];
-    maxtempArray.push([record.Lat, record.Long, record['Temp(F)']]);
-  }
-    var heat = L.heatLayer(maxtempArray, {
-        radius: 50,
-        blur: 15
-      }).addTo(map);
-    });
+    for (var i = 0; i < response.length; i++) {
+      var record = response[i];
+      maxtempArray.push([record.Lat, record.Long, record['Temp(F)']]);
+    }
+      var heat = L.heatLayer(maxtempArray, {
+          radius: 50,
+          blur: 15
+        }).addTo(map);
+      });
+};
 
 L.control.layers(baseMaps, overlayMaps, {collapsed: false}).addTo(map);
 
+};
 
-
+// init();
 // let myurl = 'http://localhost:5000/api/maxt';
 // d3.json(myurl, function(response) {
 //   var heatArray = [];
